9882fb02439167046a6ab1a3c863b2b3eb2aed18
Kaushik1575 <dask64576@gmail.com>
2026-05-29 14:04:41 +0530

    add student btn imple

--- Documentation/OPEN-DIAGRAM.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Opening diagram in your browser...
-echo Make sure "dotnet run" is running first!
-start http://localhost:5000/Home/Diagram
-timeout /t 8
--- Documentation/OPEN-IN-EDGE.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Open Data Flow Diagram in Edge
-start msedge "file:///C:/Users/dask6/OneDrive/Desktop/ITR1/Internship/Documentation/SystemDataFlowDiagram-Simple.html"
-echo Opening diagram in Microsoft Edge...
-echo If Edge did not open, read OPEN-GUIDE.txt in this folder.
-timeout /t 5
